2e4be65323726c051c1df04d0fc745d0c98926ba
Jason Balaci <jason@balaci.me>
2023-03-15 15:29:42 -0400

    Copy and paste PD Controller for my own work too :)

---
+++ code/drasil-example/bmbnd/app/Main.hs
@@ -0,0 +1,16 @@
+module Main (main) where
+
+import GHC.IO.Encoding (setLocaleEncoding, utf8)
+import Drasil.PDController.Body (printSetting, srs, fullSI)
+import Drasil.PDController.Choices (codeChoices, codeSpecs)
+import Language.Drasil.Generate (gen, typeCheckSI, genCode, genDot, genLog, DocSpec(DocSpec), DocType(SRS), Format(..), docChoices)
+
+
+main :: IO ()
+main = do
+  setLocaleEncoding utf8
+  typeCheckSI fullSI
+  gen (DocSpec (docChoices SRS [HTML, TeX]) "BmBnd_SRS") srs printSetting
+  -- genCode codeChoices codeSpecs
+  -- genDot fullSI
+  -- genLog fullSI printSetting
---
+++ code/drasil-example/bmbnd/lib/Drasil/PDController/Choices.hs
@@ -0,0 +1,27 @@
+module Drasil.PDController.Choices where
+
+import Language.Drasil.Code (AuxFile(..), Choices(..), CodeSpec, Comments(..), 
+  ConstantRepr(..), ConstantStructure(..), ConstraintBehaviour(..), 
+  ImplementationType(..), InputModule(..), Lang(..), Modularity(..), Structure(..), 
+  Verbosity(..), Visibility(..), codeSpec, defaultChoices, makeArchit, makeData, 
+  makeConstraints, makeODE, makeDocConfig, makeLogConfig, makeOptFeats, ExtLib(..))
+
+import Data.Drasil.ExternalLibraries.ODELibraries (scipyODEPckg, osloPckg,
+  apacheODEPckg, odeintPckg)
+import Drasil.PDController.Body (pidODEInfo, fullSI)
+
+codeSpecs :: CodeSpec
+codeSpecs = codeSpec fullSI codeChoices []
+
+codeChoices :: Choices
+codeChoices = defaultChoices{
+  lang = [Python, CSharp, Java, Cpp],
+  architecture = makeArchit (Modular Combined) Program,
+  dataInfo = makeData Unbundled (Store Bundled) Const,
+  optFeats = makeOptFeats
+    (makeDocConfig [CommentFunc, CommentClass, CommentMod] Verbose Hide)
+    (makeLogConfig [] "log.txt")
+    [SampleInput "../../datafiles/pdcontroller/sampleInput.txt", ReadME],
+  srsConstraints = makeConstraints Exception Exception,
+  extLibs = [Math (makeODE [pidODEInfo] [scipyODEPckg, osloPckg, apacheODEPckg, odeintPckg])]
+}
