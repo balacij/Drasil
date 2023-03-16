d704e19537a4e1cd8239844b9a00b67377383a52
Jason Balaci <jason@balaci.me>
2023-03-16 01:27:55 -0400

    Add more quantities

--- code/drasil-example/bmbnd/lib/Drasil/BmBnd/Quantities.hs
+++ code/drasil-example/bmbnd/lib/Drasil/BmBnd/Quantities.hs
@@ -11,13 +11,23 @@ quantities = map qw unitals
 inputs, outputs :: [QuantityDict]
 inputs = map qw inputUnitals
 
-outputs = []
+outputs = map qw outputUnitals
 
 unitals :: [UnitalChunk]
-unitals = [x, w_B] ++ inputUnitals
+unitals =
+  intermediateUnitals ++ abstractUnitals ++ inputUnitals ++ outputUnitals
+
+intermediateUnitals :: [UnitalChunk]
+intermediateUnitals = [x, w_B]
+
+abstractUnitals :: [UnitalChunk]
+abstractUnitals = [l, e, i, y]
 
 inputUnitals :: [UnitalChunk]
 inputUnitals = [a_0, a_1, a_2, a_3, l_B, e_B, i_B]
+
+outputUnitals :: [UnitalChunk]
+outputUnitals = [y_B]
 
 -- QUESTION: If we only store QuantityDicts, why do we bother with UnitalChunks
 --           as a type? (see: `qw x` under quantities list)
@@ -39,7 +49,7 @@ w_B = uc'
   (S "loading function")
   (sub lW cB)
   (mkFunction [Real] Real)
-  forcePerMeterU
+  newton
 
 l_B :: UnitalChunk
 l_B = uc'
@@ -68,6 +78,53 @@ i_B = uc'
   Real
   metre
 
+y_B :: UnitalChunk
+y_B = uc'
+  "y_b"
+  (nounPhraseSent $ S "deflection at a particular point along the beam")
+  (S "deflection function")
+  (sub lY cB)
+  (mkFunction [Real] Real)
+  metre
+
+l :: UnitalChunk
+l = uc'
+  "l"
+  (nounPhraseSent $ S "length of the abstract beam")
+  (S "length")
+  cL
+  Real
+  metre
+
+e :: UnitalChunk
+e = uc'
+  "e"
+  (nounPhraseSent $ S "modulus of elasticity of the abstract beam")
+  (S "modulus of elasticity")
+  cE
+  Real
+  pascal
+
+i :: UnitalChunk
+i = uc'
+  "i"
+  (nounPhraseSent
+   $ S "moment of second area of a cross-section of the abstract beam")
+  (S "moment of second area")
+  cI
+  Real
+  metre
+
+y :: UnitalChunk
+y = uc'
+  "y"
+  (nounPhraseSent
+   $ S "deflection at a particular point along the abstract beam")
+  (S "deflection function")
+  lY
+  (mkFunction [Real] Real)
+  metre
+
 -- TODO: I have 2 things to implicitly keep in mind (which I shouldn't) while I write these unital chunks:
 --       1) the strings should start with a lowercase letter, and
 --       2) the strings should not end with a period.
@@ -80,10 +137,10 @@ a_n n
     s
     (subStr lA $ show n)
     Real
-    forcePerMeterU
+    forcePerMeterU -- FIXME: Typo in stdlib? 'metre'?
   | otherwise = error "'a_n' only allows non-negative 'n's"
   where
-    s = S $ "coefficient of w_B's power " ++ show n ++ " term" -- FIXME: I'm really cheating with this formulation of text. Try re-writing it in another way! :)
+    s = S $ "coefficient of w_B's term of power " ++ show n -- FIXME: I'm really cheating with this formulation of text. Try re-writing it in another way! :)
 
 a_0, a_1, a_2, a_3 :: UnitalChunk
 a_0 = a_n 0
