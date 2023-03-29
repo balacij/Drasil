52472e4442d22ad82485659a74611ae392b3cff7
Jason Balaci <jason@balaci.me>
2023-03-28 23:30:15 -0400

    Start converting to an ODEModel

--- code/drasil-example/bmbnd/lib/Drasil/BmBnd/InstanceModels.hs
+++ code/drasil-example/bmbnd/lib/Drasil/BmBnd/InstanceModels.hs
@@ -4,33 +4,38 @@ import           Theory.Drasil
 import           Drasil.BmBnd.Quantities
 import           Language.Drasil
 import qualified Drasil.BmBnd.Assumptions as As
+import Data.Drasil.SI_Units (metre)
+import Control.Lens
+import Language.Drasil.Development (showUID)
 
 models :: [InstanceModel]
-models = [dummyDeflection, deflection]
+models = [deflection]
 
-dummyDeflection :: InstanceModel
-dummyDeflection = im
-  (equationalModel
-     "deflectionIM"
-     (nounPhraseSP "deflection as an instance model")
-     deflectionQD)
-  (map qwUC [w_B, l_B, e_B, i_B]) -- FIXME: Other than the obvious type confliction, why should I need to convert the unitals to quantitydicts
-  (qw y_B) -- FIXME: Same as above conflict
-  []
-  [dRef As.beamLoadingFunctionIntegrable]
-  Nothing -- FIXME: Derivation
-  "deflection"
-  [S "Analyzing the deflection of the beam as a boundary value problem."]
-  where
-    deflectionQD :: SimpleQDef
-    deflectionQD = mkFuncDefByQ y_B [x] (sy e_B)
+y_BCC :: ConstrConcept
+y_BCC = cuc' (showUID y_B)
+  (nounPhraseSP "temperature of the water")
+  "the average kinetic energy of the particles within the water" 
+  (eqSymb y_B) metre (Vect Real)
+  [] (exactDbl 0)
+
+deflectionDM :: DifferentialModel 
+deflectionDM = 
+  makeASystemDE
+    x
+    y_BCC
+    coeffs
+    unknowns
+    constants
+    "deflectionIM" 
+    (nounPhraseSP "deflection as an instance model") 
+    (y_B ^. defn)
+    where coeffs = [[sy e_B `mulRe` sy i_B, exactDbl 0]]
+          unknowns = [4, 0] -- FIXME: if I don't put a 0, then the program runs out of memory!
+          constants = [apply1 w_B x]
 
 deflection :: InstanceModel
 deflection = im
-  (othModel
-     "deflectionIM"
-     (nounPhraseSP "deflection as an instance model")
-     deflectionRC)
+  (newDEModel' deflectionDM)
   (map qwUC [w_B, l_B, e_B, i_B]) -- FIXME: Other than the obvious type confliction, why should I need to convert the unitals to quantitydicts
   (qw y_B) -- FIXME: Same as above conflict
   []
