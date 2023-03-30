bde5abc526c2bd3ada5fc4f3fdd5d8e2858c98f8
Jason Balaci <jason@balaci.me>
2023-03-29 21:51:20 -0400

    Revert "Create an example for the infinite recursion issue"
    This reverts commit 54f3bf3fbe73b1297f2ed6fe3e8d2bba96fa51f4.

--- code/drasil-example/bmbnd/lib/Drasil/BmBnd/InstanceModels.hs
+++ code/drasil-example/bmbnd/lib/Drasil/BmBnd/InstanceModels.hs
@@ -9,34 +9,31 @@ import Control.Lens
 import Language.Drasil.Development (showUID)
 
 models :: [InstanceModel]
-models = [deflection]
+models = [deflectionCheat, deflection]
 
-
-y_BCC :: ConstrConcept
-y_BCC = cuc' (showUID y_B)
-  (nounPhraseSP "temperature of the water")
-  "the average kinetic energy of the particles within the water" 
-  (eqSymb y_B) metre (Vect Real)
-  [] (exactDbl 0)
-
-deflectionDM :: DifferentialModel 
-deflectionDM = 
-  makeASystemDE
-    x
-    y_BCC
-    coeffs
-    unknowns
-    constants
-    "deflectionIM" 
-    (nounPhraseSP "deflection as an instance model") 
-    (y_B ^. defn)
-    where coeffs = [[sy e_B `mulRe` sy i_B]]
-          unknowns = [4] -- FIXME: if I don't put a 0, then the program runs out of memory!
-          constants = [apply1 w_B x]
+deflectionCheat :: InstanceModel
+deflectionCheat = im
+  (equationalModel
+     "deflectionIMCheat"
+     (nounPhraseSP "deflection as an instance model")
+     deflectionQD)
+  (map qwUC [w_B, l_B, e_B, i_B]) -- FIXME: Other than the obvious type confliction, why should I need to convert the unitals to quantitydicts
+  (qw y_B) -- FIXME: Same as above conflict
+  []
+  [dRef As.beamLoadingFunctionIntegrable]
+  Nothing -- FIXME: Derivation
+  "deflection"
+  [S "Analyzing the deflection of the beam as a boundary value problem."]
+  where
+    deflectionQD :: SimpleQDef
+    deflectionQD = mkQuantDef y_B (apply cheat [])
 
 deflection :: InstanceModel
 deflection = im
-  (newDEModel' deflectionDM)
+  (othModel
+     "deflectionIM"
+     (nounPhraseSP "deflection as an instance model")
+     deflectionRC)
   (map qwUC [w_B, l_B, e_B, i_B]) -- FIXME: Other than the obvious type confliction, why should I need to convert the unitals to quantitydicts
   (qw y_B) -- FIXME: Same as above conflict
   []
@@ -56,3 +53,49 @@ deflection = im
       :: ModelExpr -- FIXME: This should technically have a universal quantifier for x
     deflectionME = (sy e_B `mulRe` sy i_B `mulRe` nthderiv 4 (sy y_B) x)
       $= apply1 w_B x
+
+
+-- y_BCC :: ConstrConcept
+-- y_BCC = cuc' (showUID y_B)
+--   (nounPhraseSP "temperature of the water")
+--   "the average kinetic energy of the particles within the water" 
+--   (eqSymb y_B) metre (Vect Real)
+--   [] (exactDbl 0)
+
+-- deflectionDM :: DifferentialModel 
+-- deflectionDM = 
+--   makeASystemDE
+--     x
+--     y_BCC
+--     coeffs
+--     unknowns
+--     constants
+--     "deflectionIM" 
+--     (nounPhraseSP "deflection as an instance model") 
+--     (y_B ^. defn)
+--     where coeffs = [[sy e_B `mulRe` sy i_B, exactDbl 0]]
+--           unknowns = [4, 0] -- FIXME: if I don't put a 0, then the program runs out of memory!
+--           constants = [apply1 w_B x]
+
+-- deflection :: InstanceModel
+-- deflection = im
+--   (newDEModel' deflectionDM)
+--   (map qwUC [w_B, l_B, e_B, i_B]) -- FIXME: Other than the obvious type confliction, why should I need to convert the unitals to quantitydicts
+--   (qw y_B) -- FIXME: Same as above conflict
+--   []
+--   [dRef As.beamLoadingFunctionIntegrable]
+--   Nothing -- FIXME: Derivation
+--   "deflection"
+--   [S "Analyzing the deflection of the beam as a boundary value problem."]
+--   where
+--     deflectionRC :: RelationConcept
+--     deflectionRC = makeRC
+--       "deflectionRC"
+--       (nounPhraseSP "deflection as a relation concept")
+--       (S "deflection as a relation concept description") -- FIXME: description
+--       deflectionME
+
+--     deflectionME
+--       :: ModelExpr -- FIXME: This should technically have a universal quantifier for x
+--     deflectionME = (sy e_B `mulRe` sy i_B `mulRe` nthderiv 4 (sy y_B) x)
+--       $= apply1 w_B x
